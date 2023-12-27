50387bb402c07d0b0578d44c19d54da34ec95915
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2023-12-27 16:16:58 +0530

    Update UDF_Default.dart

--- DAY3/UDF_Default.dart
+++ DAY3/UDF_Default.dart
@@ -1,13 +1,14 @@
-void main() {
+void main()
+{
   print("\n");
   Area(10);
   print("\n");
 }
 
-void Area(int len, [int breadth = 2, int height = 15]) {
+void Area(int len, [int breadth = 2, int height = 15]) 
+{
   print("length is : $len");
   print("breadth is : $breadth");
   print("height is : $height");
-
   print("Volume : ${len * breadth * height}");
 }
