418c8592cb9df0470e50bc6f17897858d5161928
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2023-12-30 09:12:01 +0530

    Update 2.dart

--- DAY2/2.dart
+++ DAY2/2.dart
@@ -2,13 +2,14 @@ import 'dart:io';
 
 void main() {
 
-  // subtration.
+  //subtraction.
   
   stdout.write('Enter the num1 : ');
   int num1 = int.parse(stdin.readLineSync()!);
 
   stdout.write("Enter the num2 : ");
-  int num2 = int.parse(stdin.readLineSync()!);
+  int num2 = int.parse(stdin.readLineSync()!); 
+  
 
   print("The subtraction of $num1 and $num2 is ${num1 - num2}.");
 }
