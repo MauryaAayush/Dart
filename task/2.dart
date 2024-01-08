9d38fdb991ce9b14a2ed73b3df89f36e49901b43
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2024-01-08 15:09:53 +0530

    Add files via upload

---
+++ task/2.dart
@@ -0,0 +1,19 @@
+// Wap print all even no into 1 to N.
+
+import 'dart:io';
+
+void main() {
+  int num;
+
+  stdout.write("Enter the number were did you want to print even num : ");
+  num = int.parse(stdin.readLineSync()!);
+
+  for (var i = 1; i <= num; i++) {
+    if(i%2==0)
+    {
+      print(i);
+    }
+    
+  }
+}
+
