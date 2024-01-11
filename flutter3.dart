cb324c8c412d908d8533e203eeed7f1b5fb49112
Aayush <143180849+MauryaAayush@users.noreply.github.com>
2024-01-11 23:39:33 +0530

    Create flutter3.dart

---
+++ flutter3.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+
+        useMaterial3: true,
+      ),
+      home:
+      Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          leading:const Icon(Icons.menu, color: Colors.black,),
+          title:const Text('Flutter App',style: TextStyle(
+              color: Colors.black,fontWeight: FontWeight.w500
+
+          ),
+          ),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text('Red & White ', style: TextStyle(color: Colors.red,
+              fontSize: 50,fontWeight: FontWeight.w600,
+          decoration: TextDecoration.underline,
+          decorationStyle: TextDecorationStyle.double,
+          decorationThickness: 0.5,
+          decorationColor: Colors.yellow)
+          ),
+        ),
+      ),
+
+    );
+  }
+}
