13347d7a5f9ec706d6ade588ba635c97df7b321d
MauryaAayush <maayush235@gmail.com>
2024-01-19 13:19:31 +0530

    he

--- New4.2/3.dart
+++ New4.2/3.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
-class SuperMarket
-{
+class SuperMarket {
   late int itemNumber;
   late String itemName;
   late int quantity;
@@ -25,7 +24,7 @@ class SuperMarket
     discount = double.parse(stdin.readLineSync()!);
   }
 
-   void display() {
+  void display() {
     print("\nItem Details:");
     print("Item Number: $itemNumber");
     print("Item Name: $itemName");
@@ -33,11 +32,41 @@ class SuperMarket
     print("Tax: $tax%");
     print("Discount: $discount%");
   }
-
 }
 
+void main() {
+  bool verify() {
+    stdout.write("Enter User ID: ");
+    String userId = stdin.readLineSync()!;
 
-void main()
-{
-  
+    stdout.write("Enter Password: ");
+    String password = stdin.readLineSync()!;
+
+    // Simulated verification logic (replace with actual authentication logic)
+    return (userId == "Aayush" && password == "Aayush@258");
+  }
+
+  bool isAuthenticated = verify();
+
+  if (!isAuthenticated) {
+    print("Sorry !!! 😔 you have enter wrong password 😔");
+  }
+
+
+  stdout.write("Enter the number of items: ");
+  int n = int.parse(stdin.readLineSync()!);
+
+  List<SuperMarket> itemRecords = [];
+
+  for (int i = 1; i <= n; i++) {
+    SuperMarket itemRecord = SuperMarket();
+
+    print("\nEnter details for item $i:\n");
+    itemRecord.info();
+
+    itemRecords.add(itemRecord);
+  }
+
+itemRecords.sort((a, b) => a.itemNumber.compareTo(b.itemNumber));
+
 }
