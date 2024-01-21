3ed1bf7554296557c4db595488d1a494d22b99bb
MauryaAayush <maayush235@gmail.com>
2024-01-21 22:22:50 +0530

    yes

--- 4.3/convert/program.dart
+++ 4.3/convert/program.dart
@@ -5,18 +5,18 @@ class Convert {
   late int feet;
 
   void input() {
-
     stdout.write("Enter the value of feet : ");
     feet = int.parse(stdin.readLineSync()!);
 
     stdout.write("Enter the value of inches : ");
     inches = int.parse(stdin.readLineSync()!);
-    
   }
 
-  void get()
-  {
-    
+  void get() {
+    int Remain_Inches = inches ~/ 12;
+    int Main_inches = inches % 12;
 
+    print("New Feet is ${feet + Remain_Inches}");
+    print("New Inches is $Main_inches");
   }
 }
