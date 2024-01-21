68cb085c35fce798e0f609baac3f39f00bbedf99
MauryaAayush <maayush235@gmail.com>
2024-01-21 22:18:36 +0530

    h

--- 4.3/convert/program.dart
+++ 4.3/convert/program.dart
@@ -4,6 +4,19 @@ class Convert {
   late int inches;
   late int feet;
 
+  void input() {
 
+    stdout.write("Enter the value of feet : ");
+    feet = int.parse(stdin.readLineSync()!);
 
+    stdout.write("Enter the value of inches : ");
+    inches = int.parse(stdin.readLineSync()!);
+    
+  }
+
+  void get()
+  {
+    
+
+  }
 }
