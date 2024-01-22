91cc31774903e01ca566a742484062b88d9569de
MauryaAayush <maayush235@gmail.com>
2024-01-22 18:38:35 +0530

    mahadev

--- 4.3/convert/cascade.dart
+++ 4.3/convert/cascade.dart
@@ -2,11 +2,34 @@ class Cascade {
   late String name;
   late int roll_no;
 
-  void get() {
+  void get() 
+  {
     print("This one is Get method");
   }
 
-  void set() {
+  void set() 
+  {
     print("This one is set method");
   }
+
+  void myname() 
+  {
+    print("My Name is Aayush Maurya");
+  }
+
+  Cascade() 
+  {
+    print("This is a simple constructor");
+  }
+
+  Cascade.simple() 
+  {
+    print("This one is modified and simple version");
+  }
+
+  Cascade.para(String Surname) 
+  {
+    print(Surname);
+  }
+  
 }
