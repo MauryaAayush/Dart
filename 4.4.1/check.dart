c5f42054e548fb1e27d82de22e755c6812d4f014
MauryaAayush <maayush235@gmail.com>
2024-01-29 20:37:21 +0530

    Radhey Rdahey

--- 4.4.1/check.dart
+++ 4.4.1/check.dart
@@ -1,17 +1,32 @@
 class Bank {
-  late int Id, age, Phone;
-  late String name, Surname;
+  late bool isActive;
+  late int age;
+  late String balance;
+  late String eyeColor;
+  late String name;
+  late String gender;
+  late String company;
+  late String email;
+  late String phone;
+  late String address;
 
-  Bank._temp(this.Id, this.age, this.Phone, this.name, this.Surname);
+  Bank._temp(this.isActive, this.age, this.balance, this.eyeColor, this.name,
+      this.gender, this.company, this.email, this.phone, this.address);
 
   factory Bank.fromMap(Map m1) {
-    int Id = m1['Id'];
+    bool isActive = m1['isActive'];
+    String balance = m1['balance'];
     int age = m1['age'];
-    int Phone = m1['Phone'];
+    String eyeColor = m1['eyeColor'];
     String name = m1['name'];
-    String Surname = m1['Surname'];
+    String gender = m1['gender'];
+    String company = m1['company'];
+    String email = m1['email'];
+    String phone = m1['phone'];
+    String address = m1['address'];
 
-    Bank p1 = Bank._temp(Id, age, Phone, name, Surname);
+    Bank p1 = Bank._temp(isActive, age, balance, eyeColor, name, gender,
+        company, email, phone, address);
 
     return p1;
   }
