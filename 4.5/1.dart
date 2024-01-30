d3957381161ffc524d9b5a1930f53a8b42758918
MauryaAayush <maayush235@gmail.com>
2024-01-30 23:31:06 +0530

    Radhey Radhey

--- 4.5/1.dart
+++ 4.5/1.dart
@@ -1,20 +1,18 @@
 import 'dart:io';
 
 void main() {
+  
+  try {
+  
   int num1, num2;
   stdout.write("enter the value of number 1:");
   num1 = int.parse(stdin.readLineSync()!);
   stdout.write("enter the value of number 2:");
   num2 = int.parse(stdin.readLineSync()!);
 
-  int ans = num1 ~/ num2;
-
-  try {
-    if (num2 == 0) {
-      throw 404;
-    } else {
+      int ans = num1 ~/ num2;
       print("ansner=$ans");
-    }
+    
   }
    on IntegerDivisionByZeroException
   {
---
+++ 4.5/4.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  late int num1, num2;
+
+  try {
+    stdout.write("Enter the value of number 1: ");
+    num1 = int.parse(stdin.readLineSync()!);
+
+    stdout.write("Enter the value of number 2: ");
+    num2 = int.parse(stdin.readLineSync()!);
+
+    int ans = num1 ~/ num2;
+    print("Answer: $ans");
+  } catch (e) {
+    print("You have enter $num2 which is not divisible with $num1 ");
+  }
+}
