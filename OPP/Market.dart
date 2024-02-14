00acb54b24a88cff6e1646d8b6740b307bc8a5d4
MauryaAayush <maayush235@gmail.com>
2024-02-14 19:20:31 +0530

    Radhey Radhey

--- OPP/Market.dart
+++ OPP/Market.dart
@@ -28,8 +28,7 @@ void main() {
       case 2:
         print("\n! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ");
 
-        for (int i = 0; i < customerList.length; i++) 
-        {
+        for (int i = 0; i < customerList.length; i++) {
           customerList[i]['customerInfo'].Output();
           All_Product.Cart_Data(customerList[i]['cartData']);
         }
@@ -44,8 +43,7 @@ void main() {
         searchId = int.parse(stdin.readLineSync()!);
         print("- - - - - - - - - - - - - - - - - -");
 
-        for (int i = 0; i < customerList.length; i++) 
-        {
+        for (int i = 0; i < customerList.length; i++) {
           if (searchId == customerList[i]['customerInfo'].TakeId()) {
             found = true;
             customerList[i]['customerInfo'].Output();
@@ -73,16 +71,16 @@ void main() {
 
 class All_Product {
   List ProductList = [
-    {"Pr.id": 1, "Pr.name": "Sugar", "Price": 50, "Quantity": 0},
-    {"Pr.id": 2, "Pr.name": "Pulses", "Price": 120, "Quantity": 0},
-    {"Pr.id": 3, "Pr.name": "Rice", "Price": 1200, "Quantity": 0},
-    {"Pr.id": 4, "Pr.name": "Kaju", "Price": 600, "Quantity": 0},
-    {"Pr.id": 5, "Pr.name": "Almond", "Price": 700, "Quantity": 0},
-    {"Pr.id": 6, "Pr.name": "Kismis", "Price": 150, "Quantity": 0},
-    {"Pr.id": 7, "Pr.name": "Tea", "Price": 50, "Quantity": 0},
-    {"Pr.id": 8, "Pr.name": "coffee", "Price": 70, "Quantity": 0},
-    {"Pr.id": 9, "Pr.name": "Bread", "Price": 50, "Quantity": 0},
-    {"Pr.id": 10, "Pr.name": "Jam", "Price": 185, "Quantity": 0},
+    {"Pr.id": 1, "Pr.name": "Sugar", "Price": 50, "Quantity": 1},
+    {"Pr.id": 2, "Pr.name": "Pulses", "Price": 120, "Quantity": 1},
+    {"Pr.id": 3, "Pr.name": "Rice", "Price": 1200, "Quantity": 1},
+    {"Pr.id": 4, "Pr.name": "Kaju", "Price": 600, "Quantity": 1},
+    {"Pr.id": 5, "Pr.name": "Almond", "Price": 700, "Quantity": 1},
+    {"Pr.id": 6, "Pr.name": "Kismis", "Price": 150, "Quantity": 1},
+    {"Pr.id": 7, "Pr.name": "Tea", "Price": 50, "Quantity": 1},
+    {"Pr.id": 8, "Pr.name": "coffee", "Price": 70, "Quantity": 1},
+    {"Pr.id": 9, "Pr.name": "Bread", "Price": 50, "Quantity": 1},
+    {"Pr.id": 10, "Pr.name": "Jam", "Price": 185, "Quantity": 1},
   ];
 
   List cartList = [];
@@ -110,12 +108,16 @@ class All_Product {
     stdout.write("Enter your product : ");
     Choose_Product = int.parse(stdin.readLineSync()!);
 
-    if (Choose_Product <= 10 && Choose_Product > 0) {
+    if (Choose_Product <= 10 && Choose_Product > 0  ) {
       map['Pr.id'] = ProductList[Choose_Product - 1]['Pr.id'];
       map['Pr.name'] = ProductList[Choose_Product - 1]['Pr.name'];
       map['Price'] = ProductList[Choose_Product - 1]['Price'];
 
-      map['quantity'] = Choose_Product_Quantity;
+      stdout.write("Enter the number of Quantity : ");
+      Choose_Product_Quantity = int.parse(stdin.readLineSync()!);
+      print("- - - - - - - - - - - - - - - - - -\n");
+
+      map['Quantity'] = Choose_Product_Quantity;
 
       cartList.add(map);
 
@@ -148,21 +150,22 @@ class All_Product {
           "Name : ${list[i]['Pr.name']}, Price : ${list[i]['Price']}Rs , Quantity : ${list[i]['Quantity']}");
     }
 
-    if (Total_Amount >= 500 && Total_Amount <= 1500) {
+    if (Total_Amount >= 500 && Total_Amount <= 1510) {
       Discount = 10;
-    } else if (Total_Amount > 1500 && Total_Amount <= 3500) {
+    } else if (Total_Amount > 1510 && Total_Amount <= 3510) {
       Discount = 20;
-    } else if (Total_Amount > 3500 && Total_Amount <= 6500) {
+    } else if (Total_Amount > 3510 && Total_Amount <= 6510) {
       Discount = 25;
-    } else if (Total_Amount > 6500) {
+    } else if (Total_Amount > 6510) {
       Discount = 30;
     }
+
 
     Discount_Amount = Total_Amount * (Discount / 100);
     Payable_Amount = Total_Amount - Discount_Amount;
 
     print("\n Total Amount ->  $Total_Amount");
-    print("\n Discount Amount ->  $Discount_Amount");
+    print("\n Discount Amount ->  $Discount_Amount  ($Discount%)");
     print("\n Payable Amount ->  $Payable_Amount");
 
     print("\n - * - * - * - * - * - * - * - * - * - * - * - *\n");
